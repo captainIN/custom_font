ad179753cf4c705935a6b6566a5683032687fb9b
Chouhans <jaymanyoo3@gmail.com>
2021-08-30 01:02:42 +0530

    Completed

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import { typography } from './typography';
+typography()
+AppRegistry.registerComponent(appName, () => App);
